d885e11c34c23b5e24905e49a0b2ccd50d8b92ad
Olyad Negero <olyadnegero@gmail.com>
2026-03-21 17:42:14 +0300

    modification: the entire structure of '/frontend' folder is changed and modified

--- frontendd/olyad-portfolio-website/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import "../src/app/styles/index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
